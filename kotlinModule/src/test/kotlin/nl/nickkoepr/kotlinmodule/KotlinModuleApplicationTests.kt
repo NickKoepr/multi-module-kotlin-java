54551b67b482386359df736f3665cb201616ecb8
Nick Kuiper <nickkuiper44@gmail.com>
2024-09-12 16:40:04 +0200

    Initial commit

---
+++ kotlinModule/src/test/kotlin/nl/nickkoepr/kotlinmodule/KotlinModuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.nickkoepr.kotlinmodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinModuleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
