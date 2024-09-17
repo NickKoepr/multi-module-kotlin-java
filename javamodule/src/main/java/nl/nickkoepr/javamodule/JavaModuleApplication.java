5d2dc5b68ae6b9cfbe1f3351de87613906538c74
Nick Kuiper <nickkuiper44@gmail.com>
2024-09-17 16:22:28 +0200

    Changed module names to only include lower case letters.

---
+++ javamodule/src/main/java/nl/nickkoepr/javamodule/JavaModuleApplication.java
@@ -0,0 +1,13 @@
+package nl.nickkoepr.javamodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaModuleApplication.class, args);
+    }
+
+}
